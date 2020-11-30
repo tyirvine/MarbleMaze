26c2259b229837ad9997f12a1363434778c39ba3
AlexMPester <47048642+AlexMPester@users.noreply.github.com>
2020-11-29 22:50:19 -0500

    Initial commit

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+      public Transform destination;
+    public float distance = 1f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, destination.position) <= distance)
+        {
+            Debug.Log("HERE");
+        }
+    }
+}
---
+++ Assets/Scripts/moveBoard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class moveBoard : MonoBehaviour
+{
+
+    //Defining Variables
+    public float turnSpeed = 25f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //These two move on the Z-Axis
+       if(Input.GetKey(KeyCode.UpArrow))
+            transform.Rotate(Vector3.back, turnSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.DownArrow))
+            transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+
+        //These two move on the X-Axis
+        if(Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.right, turnSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.left, turnSpeed * Time.deltaTime);
+    }
+}
