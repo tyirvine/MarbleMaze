4474c6bbc21508e55adc93da4912eec67b612cea
Ty Irvine <39813066+tyirvine@users.noreply.github.com>
2020-11-30 21:46:01 -0700

    Added in floor meshes and fixed the fall through the map bug

--- Assets/Scripts/moveBoard.cs
+++ Assets/Scripts/moveBoard.cs
@@ -2,28 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class moveBoard : MonoBehaviour {
 
-public class moveBoard : MonoBehaviour
-{
+	//Turn speed dictates how quickly the board should rotate
+	public float turnSpeed = 25f;
 
-    //Defining Variables
-    public float turnSpeed = 25f;
+	void FixedUpdate() {
+		//transform.rotation = Quaternion.Euler(transform.rotation.x, 0f, transform.rotation.z);
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        //These two move on the Z-Axis
-       if(Input.GetKey(KeyCode.UpArrow))
-            transform.Rotate(Vector3.back, turnSpeed * Time.deltaTime);
-        
-        if(Input.GetKey(KeyCode.DownArrow))
-            transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+		//These two move on the Z-Axis
+		if (Input.GetKey(KeyCode.UpArrow))
+			transform.Rotate(Vector3.back, turnSpeed * Time.deltaTime);
 
-        //These two move on the X-Axis
-        if(Input.GetKey(KeyCode.LeftArrow))
-            transform.Rotate(Vector3.right, turnSpeed * Time.deltaTime);
-        
-        if(Input.GetKey(KeyCode.RightArrow))
-            transform.Rotate(Vector3.left, turnSpeed * Time.deltaTime);
-    }
+		if (Input.GetKey(KeyCode.DownArrow))
+			transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+
+		//These two move on the X-Axis
+		if (Input.GetKey(KeyCode.LeftArrow))
+			transform.Rotate(Vector3.right, turnSpeed * Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.RightArrow))
+			transform.Rotate(Vector3.left, turnSpeed * Time.deltaTime);
+	}
+
 }
