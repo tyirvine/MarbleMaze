05dc4b3ed4f0d769f203b0f0b80a55ad45e689fe
Ty Irvine <39813066+tyirvine@users.noreply.github.com>
2020-12-17 17:23:22 -0700

    Organized the project a bit and updated the InputActionControls
    Co-Authored-By: bubzy-coding <74142528+bubzy-coding@users.noreply.github.com>

--- Assets/Scripts/Z - Board/BoardMove.cs
+++ Assets/Scripts/Z - Board/BoardMove.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BoardMove : MonoBehaviour {
 
 	//Turn speed dictates how quickly the board should rotate
 	public float turnSpeed = 25f;
 
+	// Choose whether or not to show the axis wheels
+	public bool showAxisWheels;
+
+	// These are objects that indicate how much the board is turning
+	public GameObject xAxisWheel;
+	public GameObject zAxisWheel;
+
+	// This just holds the input rotation provided by the OnMove event
+	Vector2 inputRotation = new Vector2(0f, 0f);
+
 	void FixedUpdate() {
-		// Controlled by the Input Manager found in Project Settings > Input Manager
-		transform.Rotate(Vector3.back, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime);
-		transform.Rotate(Vector3.right, Input.GetAxis("Vertical") * turnSpeed * Time.deltaTime);
+		// Board Movement
+		transform.Rotate(Vector3.back, inputRotation.x * turnSpeed * Time.deltaTime);
+		transform.Rotate(Vector3.right, inputRotation.y * turnSpeed * Time.deltaTime);
+
+		// Movement Indicators
+		if (showAxisWheels) {
+			xAxisWheel.transform.Rotate(Vector3.forward, inputRotation.x * -turnSpeed * Time.deltaTime);
+			zAxisWheel.transform.Rotate(Vector3.forward, inputRotation.y * -turnSpeed * Time.deltaTime);
+		}
+	}
+
+	// This is being fired by the PlayerInputs component
+	public void OnMove(InputValue value) {
+		inputRotation = value.Get<Vector2>();
 	}
 
 	void Update() {
