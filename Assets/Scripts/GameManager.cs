07362ace58bfa8d3eb015508f7240efacbbb42dd
Ty Irvine <39813066+tyirvine@users.noreply.github.com>
2021-01-26 23:23:18 -0700

    Organized script workspace

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     // References
     public PathManager pathManager;
     public GameObject marblePrefab;
@@ -11,27 +10,22 @@ public class GameManager : MonoBehaviour
     bool buildNewBoard = false;
 
     /// <summary>Destroys the old board via tags.</summary>
-    public void DeleteOldBoards()
-    {
+    public void DeleteOldBoards() {
         GameObject[] wallTiles = GameObject.FindGameObjectsWithTag("boardObjects");
-        if (wallTiles.Length > 0)
-        {
-            foreach (GameObject go in wallTiles)
-            {
+        if (wallTiles.Length > 0) {
+            foreach (GameObject go in wallTiles) {
                 Destroy(go);
             }
         }
     }
 
     /// <summary>Find the board's finish hole collider.</summary>
-    public bool FindFinishHoleCollider()
-    {
+    public bool FindFinishHoleCollider() {
         return pathManager.GetComponent<BuildBoard>().pathFinishHole.GetComponent<BoxCollider>();
     }
 
     /// <summary>Just a simple script to spawn the marble.</summary>
-    void PlaceMarble()
-    {
+    void PlaceMarble() {
         marble = Instantiate(marblePrefab, pathManager.gridPoints.startPointNode, Quaternion.identity);
     }
 
@@ -39,28 +33,23 @@ public class GameManager : MonoBehaviour
     Vector3Int GetMarblePosition() => Vector3Int.FloorToInt(marble.transform.position - new Vector3Int(0, 10, 0));
 
     /// <summary>Calls the new board method after a set number of seconds.</summary>
-    public void CallForNewBoard()
-    {
+    public void CallForNewBoard() {
         Invoke("NewBoard", 2);
     }
 
     /// <summary>This method generates a new board and anything else that needs to happen.</summary>
-    public void NewBoard()
-    {
+    public void NewBoard() {
         DeleteOldBoards();
         Debug.Log("Create a new board!");
-
     }
 
     // Ensures the marble is placed before any functions occur that rely on it's position
-    private void Awake()
-    {
+    private void Awake() {
         PlaceMarble();
     }
 
     // TODO: Remove, testing only
-    private void Start()
-    {
+    private void Start() {
         pathManager.ConstructPathStack(GetMarblePosition());
     }
 
