898c79716e8c7799bb7bf0787e99b0648de180eb
bubzy-coding <74142528+bubzy-coding@users.noreply.github.com>
2021-02-02 01:46:10 +0000

    HAZARDS!! added and some modifications to shapemanager

---
+++ Assets/Scripts/Z - Hazards/BumperHazard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperHazard : MonoBehaviour
+{
+    public float bumperForce = 5f;
+    public LayerMask layerToInteract;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((layerToInteract.value & 1 << collision.gameObject.layer) == 1 << collision.gameObject.layer)
+        {
+            Debug.Log("bingo");
+            foreach (ContactPoint contact in collision.contacts)
+            {
+                contact.otherCollider.attachedRigidbody.AddForce((-1 * contact.normal)*bumperForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
