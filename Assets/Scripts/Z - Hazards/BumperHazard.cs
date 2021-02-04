fd30453b3a7c869b46538d0ee19ee4525f7cebbc
Ty Irvine <39813066+tyirvine@users.noreply.github.com>
2021-02-03 19:47:21 -0700

    Added in bumper model!
    Unity made some .meta changes. Animations are working great!

--- Assets/Scripts/Z - Hazards/BumperHazard.cs
+++ Assets/Scripts/Z - Hazards/BumperHazard.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class BumperHazard : MonoBehaviour
 {
+    // References
     public float bumperForce = 5f;
     public LayerMask layerToInteract;
-    // Update is called once per frame
-    void Update()
-    {
+    public Animator animator;
 
-    }
     private void OnCollisionEnter(Collision collision)
     {
         if ((layerToInteract.value & 1 << collision.gameObject.layer) == 1 << collision.gameObject.layer)
         {
-            // Debug.Log("bingo");
+            // This causes the animator to run the bumper animation
+            animator.SetBool("Bumped", true);
+
             foreach (ContactPoint contact in collision.contacts)
             {
                 contact.otherCollider.attachedRigidbody.AddForce((-1 * contact.normal) * bumperForce, ForceMode.Impulse);
             }
         }
     }
+
+    // Returns bumper back to idle state
+    private void OnCollisionExit(Collision other)
+    {
+        animator.SetBool("Bumped", false);
+    }
+
 }
