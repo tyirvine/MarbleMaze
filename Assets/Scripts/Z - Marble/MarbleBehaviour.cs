207d1ddf7f92732be5c39b3ea9d69a0823de3bed
bubzy-coding <74142528+bubzy-coding@users.noreply.github.com>
2021-02-04 00:36:56 +0000

    audio fixes, and added pixelate shader to Main Camera

--- Assets/Scripts/Z - Marble/MarbleBehaviour.cs
+++ Assets/Scripts/Z - Marble/MarbleBehaviour.cs
@@ -10,9 +10,13 @@ public class MarbleBehaviour : MonoBehaviour
     
     AudioSource audioSource;
     public AudioClip impact;
-
+    public AudioClip levelFinish;
+    public Collision oldCollision;
+    int layerMask = 1 << 9;
     private void Start()
     {
+     
+      //  layerMask = ~layerMask;
         audioSource = GetComponent<AudioSource>();
         // Find references
         colliders = gameObject.GetComponents<SphereCollider>();
@@ -32,17 +36,25 @@ public class MarbleBehaviour : MonoBehaviour
 
         if (other.CompareTag("LevelFinish"))
         {
+            audioSource.clip = levelFinish;
+            audioSource.PlayOneShot(levelFinish);
             gameManager.CallForNewBoard();
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.relativeVelocity.magnitude > audioTriggerSpeed)
+
+
+        Vector3 collisionForce = collision.impulse / Time.fixedDeltaTime;
+        if (collisionForce.magnitude > audioTriggerSpeed && collision.gameObject.CompareTag("wallTile")) 
             {
                 audioSource.clip = impact;
                 audioSource.PlayOneShot(impact);
+            
+            
             }
+        
 
     }
 }
