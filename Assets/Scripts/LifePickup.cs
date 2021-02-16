cf5093e4c94451c37ea8729584103e1926f8d2d3
AlexMPester <47048642+AlexMPester@users.noreply.github.com>
2021-02-15 23:46:44 -0500

    Pickup Clean up

--- Assets/Scripts/LifePickup.cs
+++ Assets/Scripts/LifePickup.cs
@@ -1,10 +1,17 @@
-﻿
+﻿using System.Globalization;
 using UnityEngine;
 
 public class LifePickup : MonoBehaviour
 {
     // References
     StatsManager stats;
+    public ParticleSystem particle;
+    public MeshRenderer meshRenderer;
+    [Range (0f,10f)] public float time; 
+
+    // Audio Settings 
+    [Header("Audio")]
+    public AudioSource pickupSound;
 
     // Grab stats object
     private void Start()
@@ -12,13 +19,26 @@ public class LifePickup : MonoBehaviour
         stats = FindObjectOfType<StatsManager>();
     }
 
-    // Add life when the player touches self
+    // Add life when the player touches self (Add Life, Turn off Mesh, Play Audio, Run Particle State, Destroy)
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             stats.AddLife();
-            Destroy(gameObject);
+            //pickupSound.Play();
+            Debug.Log("Audio Players here");
+            
+            meshRenderer.enabled = false; 
+            
+            particleState();
+            Destroy(gameObject, time);
         }
+    }
+
+    private void particleState()
+    {
+        particle.Play();
+        particle.loop = false;
+    
     }
 }
