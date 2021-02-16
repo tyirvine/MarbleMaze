b52c88d2b2df5c505fa81aa114af62443dbeb9b0
Ty Irvine <39813066+tyirvine@users.noreply.github.com>
2021-02-16 11:48:44 -0700

    Fixed some issues and started working on the UI

---
+++ Assets/Scripts/Z - User Interface/UIManager.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    /// <summary>Just a class for each menu to use. Designated for UIs only.</summary>
+    class UIMenu
+    {
+        bool isActive;
+    }
+
+    /* ------------------------------ State objects ----------------------------- */
+    UIMenu startMenu = new UIMenu();
+    UIMenu gameoverMenu = new UIMenu();
+    UIMenu pauseMenu = new UIMenu();
+
+    // Start menu
+
+    // Game over menu
+
+    // Pause menu
+
+}
