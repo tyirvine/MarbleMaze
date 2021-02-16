4697af99042d012df716bb95d9d55c04e1454f90
Ty Irvine <39813066+tyirvine@users.noreply.github.com>
2021-02-16 14:14:49 -0700

    UI is semi functional

--- Assets/Scripts/Z - User Interface/UIManager.cs
+++ Assets/Scripts/Z - User Interface/UIManager.cs
@@ -1,23 +1,63 @@
 ﻿
+using System;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     /// <summary>Just a class for each menu to use. Designated for UIs only.</summary>
-    class UIMenu
+    [Serializable]
+    public class UIMenu
     {
-        bool isActive;
+        public GameObject canvas;
+        bool isActive { get => canvas.activeInHierarchy; }
+
+        // Constructor
+        public UIMenu(bool isActive)
+        {
+            this.canvas.SetActive(false);
+        }
     }
 
-    /* ------------------------------ State objects ----------------------------- */
-    UIMenu startMenu = new UIMenu();
-    UIMenu gameoverMenu = new UIMenu();
-    UIMenu pauseMenu = new UIMenu();
+    /* ------------------------------ Menu Control ----------------------------- */
+    public UIMenu startMenu = new UIMenu(true);
+    public UIMenu gameoverMenu;
+    public UIMenu pauseMenu;
+    public UIMenu creditsMenu;
+    public UIMenu optionsMenu;
+
+    /// <summary>Opens up a specified menu object.</summary>
+    public void MenuControl(UIMenu menu, bool state) => menu.canvas.SetActive(state);
 
     // Start menu
+    public void StartMenu(bool state)
+    {
+        MenuControl(startMenu, state);
+    }
+
+    // Options menu
+    public void OptionsMenu(bool state)
+    {
+        MenuControl(optionsMenu, state);
+    }
 
     // Game over menu
+    public void GameoverMenu(bool state)
+    {
+        MenuControl(gameoverMenu, state);
+    }
 
     // Pause menu
+    public void PauseMenu(bool state)
+    {
+        MenuControl(pauseMenu, state);
+    }
+
+    // Credits menu
+    public void CreditsMenu(bool state)
+    {
+        MenuControl(creditsMenu, state);
+    }
+
+    /* ------------------------------ Miscellaneous ----------------------------- */
 
 }
