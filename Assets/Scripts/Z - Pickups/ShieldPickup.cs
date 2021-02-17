160bc933c3a5fe8746a996dc5d39a8f6a9621b3c
AlexMPester <47048642+AlexMPester@users.noreply.github.com>
2021-02-17 15:43:22 -0500

    2.3 Adjustments
    Merged 2.1 to 2.3, Added Health Pick up, Created new Script for Shield, Added Material for Spike

---
+++ Assets/Scripts/Z - Pickups/ShieldPickup.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldPickup : MonoBehaviour
+{
+    // References
+    StatsManager stats;
+    public SpikeHazard SpikeHazard;
+    public ParticleSystem particle;
+    public MeshRenderer meshRenderer;
+    public Collider collider;
+    [Range(0f, 10f)] public float time;
+
+    // Audio Settings 
+    [Header("Audio")]
+    public AudioSource pickupSound;
+
+    // Grab stats object
+    private void Start()
+    {
+        stats = FindObjectOfType<StatsManager>();
+    }
+
+    // Add life when the player touches self (Add Life, Turn off Mesh, Play Audio, Run Particle State, Destroy)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            stats.AddLife();
+            pickupSound.Play();
+            MarbleState();
+            ParticleState();
+            Destroy(gameObject, time);
+        }
+    }
+
+    private void ParticleState()
+    {
+        particle.Play();
+        particle.loop = false;
+    }
+
+    private void MarbleState()
+    {
+        SpikeHazard.marble.DeathSequenceExplode(0f);
+    }
+}
